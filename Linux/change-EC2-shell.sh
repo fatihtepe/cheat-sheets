b6577b224fa68f056f88da6d1f2aeddcaf56abac
Fatih Tepe <fatihtepe@gmail.com>
2021-06-17 09:33:56 -0400

    update change EC2 shell

--- Linux/change-EC2-shell.sh
+++
@@ -1,7 +0,0 @@
-- sudo yum install git
-- sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-- zsh
-
-or 
-
-- export PS1="\[\e[36m\]\u\[\e[m\]@\h-\w:\[\e[31m\]\\$\[\e[m\] "
