1f5eff1a0cb5ae0b4d6e6f2cf49e5f3dad256fe1
Fatih Tepe <fatihtepe@gmail.com>
2021-06-17 09:17:44 -0400

    add zsh to EC2

---
+++ Linux/change-EC2-shell.sh
@@ -0,0 +1,7 @@
+- sudo yum install git
+- sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+- zsh
+
+or 
+
+- export PS1="\[\e[36m\]\u\[\e[m\]@\h-\w:\[\e[31m\]\\$\[\e[m\] "
